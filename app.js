a4a55c7edbf6e0b3fdda48c48b5e0135fe68071a
Katelynn <mccy.k@aol.com>
2023-06-15 11:53:14 -0700

    completed files

---
+++ app.js
@@ -0,0 +1,45 @@
+// console.log("Let's get this party started!");
+
+const gifs = document.querySelector('#gifs');
+const form = document.querySelector('#searchForm');
+const input = document.querySelector('#search');
+const removeBtn = document.querySelector('#remove');
+
+async function searchGif(searchTerm){
+    if(searchTerm === ''){
+        return;
+    }
+try{
+    const res = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`);
+    // console.log(res);
+
+    const gifUrl = res.data.data[0].images.downsized_medium.url;
+    // console.log(gifUrl);
+
+    const gifImg = document.createElement('img');
+    gifImg.setAttribute('src', gifUrl);
+
+    gifs.appendChild(gifImg);
+}catch(e){
+    alert('please try a different search');
+}
+    
+}
+
+
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    // console.log(input.value);
+    searchGif(input.value);
+    input.value = '';
+});
+
+
+removeBtn.addEventListener('click', function(e){
+    gifs.innerHTML = '';
+});
+
+
+
+
